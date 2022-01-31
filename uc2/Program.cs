54dbbc699ddb5e229632a309705b295c20c82cc5
vgj24 <vrushlijoshi24@gmail.com>
2022-01-31 20:03:44 +0530

    gambler_looses_or_win

---
+++ uc2/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GamblerProblem;
+
+Console.WriteLine("Hello, World!");
+Gamblerpgm gb=new Gamblerpgm();
+gb.BetMethod();
+gb.WinLoose();
