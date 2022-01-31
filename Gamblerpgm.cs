519c76bcb6e81101bacacbe3926735dd9e03b2da
vgj24 <vrushlijoshi24@gmail.com>
2022-01-31 18:40:36 +0530

    GamlerProblem initialization

---
+++ Gamblerpgm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamblerProblem
+{
+    internal class Gamblerpgm
+    {
+        int Totalstakes = 100;
+       public static  Random random = new Random();
+       int StakePerDay = random.Next(0, 1);
+        public void BetMethod()
+        { if(StakePerDay ==100)
+            {
+                Console.WriteLine("you have reached the limit");
+            }
+            else
+            {
+                Console.WriteLine("you can play more");
+            }
+        }
+    
+    }
+}
